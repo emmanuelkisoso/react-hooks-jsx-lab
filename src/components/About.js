5c89384ae044f1b6e4f474cec0aac32ac97a08ef
emmanuel <emmanuel.kisoso@student.moringaschool.com>
2024-04-24 22:10:33 +0300

    Final Commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>Wow</p>
+    <img src={image} alt="I Made This" />
+  </div>
+  )
 }
 
 export default About;
